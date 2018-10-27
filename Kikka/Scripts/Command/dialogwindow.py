4a87a753f3203bf7a08a07cd2d3efad0c205ed16
A.A <aa13058219642@sohu.com>
2018-10-27 15:34:20 +0800

    dialog2

--- Kikka/Scripts/Command/dialogwindow.py
+++ Kikka/Scripts/Command/dialogwindow.py
@@ -3,7 +3,7 @@ import logging
 
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt, QRect, QPoint, QSize
-from PyQt5.QtGui import QPixmap, QPainter, QColor, QImage
+from PyQt5.QtGui import QPixmap, QPainter, QColor, QImage, QPalette, QBrush
 from PyQt5.QtWidgets import QWidget, QPushButton, QStackedLayout, QVBoxLayout, QHBoxLayout, QLabel, QGridLayout
 
 import kikkahelper
@@ -15,12 +15,19 @@ class Dialog(QWidget):
         self._parent = parent
         QWidget.__init__(self)
         self.setWindowFlags(Qt.Tool | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-        # self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
         # self.setAttribute(Qt.WA_DeleteOnClose)
         # self.setMouseTracking(True)
         # self.setAcceptDrops(True)
 
         self._isChangeSizeMode = False
+        self._backgroundSource = None
+        self._backgroundImage = None
+        self._backgroundPixmap = None
+        self._backgroundMask = None
+        self._backgroundRect = None
+
+        self.setBackgroundImage()
         style = '''
         QPushButton{
             border-style: solid;
@@ -98,7 +105,7 @@ class Dialog(QWidget):
     def setChangeSizeMode(self):
         self._isChangeSizeMode = not self._isChangeSizeMode
         if self._isChangeSizeMode is True:
-            #self.clearMask()
+            self.clearMask()
             self.setWindowFlag(Qt.FramelessWindowHint, False)
             self.setWindowOpacity(0.8)
             self.setEnabled(False)
@@ -121,11 +128,11 @@ class Dialog(QWidget):
             self.setWindowFlag(Qt.FramelessWindowHint, True)
             self.setWindowOpacity(1)
             self.setEnabled(True)
-            #self.setMask(self._mask)
+            self.setMask(self._backgroundMask)
             self.show()
             self.move(pos.x(), pos.y())
 
-    def updatePosition(self, offset=None):
+    def updatePosition(self):
         if self._isChangeSizeMode is True:
             return
 
@@ -153,6 +160,7 @@ class Dialog(QWidget):
             if x < 0: x = p_pos.x() + p_size.width()
 
         super().move(x, y)
+        self.getBackgroundImage()
         pass
 
     def show(self):
@@ -165,5 +173,76 @@ class Dialog(QWidget):
         event.ignore()
         pass
 
+    def resizeEvent(self, a0: QtGui.QResizeEvent):
+        logging.info("resizeEvent")
+        self.getBackgroundImage()
 
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(self.rect(), self._backgroundPixmap)
+        super().paintEvent(event)
+
+    def setBackgroundImage(self, image=None):
+        self._backgroundSource = QImage(r"C:\\d2.png")
+        w0 = self._backgroundSource.size().width() // 3
+        h0 = self._backgroundSource.size().height() // 3
+        sw = [w0, w0, w0]
+        sh = [h0, h0, h0]
+
+        srect = []
+        for y in range(3):
+            sr = []
+            for x in range(3):
+                pt = QPoint(0, 0)
+                if x > 0: pt.setX(sr[x-1].x() + sw[x-1])
+                if y > 0: pt.setY(srect[y-1][0].y() + sh[y-1])
+                sz = QSize(sw[x], sh[y])
+                sr.append(QRect(pt, sz))
+                pass
+            srect.append(sr)
+        pass  # exit for
+
+        self._backgroundRect = srect
+
+    def getBackgroundImage(self):
+        #logging.info("getBackgroundImage: (%d, %d)" % (sz.width(), sz.height()))
+
+        self._backgroundImage = QImage(self.size(), QImage.Format_ARGB32)
+        pixmap = QPixmap().fromImage(self._backgroundSource, Qt.AutoColor)
+
+        # ssw = self._backgroundSource.size().width()
+        # bsh = self._backgroundSource.size().height()
+
+        w0 = self._backgroundSource.size().width() // 3
+        h0 = self._backgroundSource.size().height() // 3
+        dw = [w0, self.size().width()-w0*2, w0]
+        dh = [h0, self.size().height()-h0*2, h0]
+
+        drect = []
+        for y in range(3):
+            dr = []
+            for x in range(3):
+                pt = QPoint(0, 0)
+                if x > 0: pt.setX(dr[x-1].x() + dw[x-1])
+                if y > 0: pt.setY(drect[y-1][0].y() + dh[y-1])
+                sz = QSize(dw[x], dh[y])
+                dr.append(QRect(pt, sz))
+                pass
+            drect.append(dr)
+        pass  # exit for
+
+        p = QPainter(self._backgroundImage)
+        p.setCompositionMode(QPainter.CompositionMode_Source)
+
+        # if self._isChangeSizeMode is True:
+        #     p.fillRect(self.rect(), Qt.black)
+
+        for y in range(3):
+            for x in range(3):
+                p.drawPixmap(drect[y][x], pixmap, self._backgroundRect[y][x])
+        p.end()
+
+        self._backgroundPixmap = QPixmap().fromImage(self._backgroundImage, Qt.AutoColor)
+        self._backgroundMask = self._backgroundPixmap.mask()
+        pass
 
